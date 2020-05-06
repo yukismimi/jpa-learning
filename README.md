c027e5ba325e2ac8749ecc8fc0eedf4157d2a42e
Yukismimi <fz_saber@qq.com>
2020-05-06 18:06:27 +0800

    add example query

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 JPA级联相关用法可参照
 * [Many-To-Many Relationship in JPA](https://www.baeldung.com/jpa-many-to-many)
+
+JPA Example查询可参考
+* [Spring Data JPA Query by Example](https://www.baeldung.com/spring-data-query-by-example)
--- src/main/java/com/yukismimi/jpalearning/controller/Controller.java
+++ src/main/java/com/yukismimi/jpalearning/controller/Controller.java
@@ -8,6 +8,8 @@ import com.yukismimi.jpalearning.repository.CourseRepository;
 import com.yukismimi.jpalearning.repository.StudentRepository;
 import com.yukismimi.jpalearning.service.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,5 +50,25 @@ public class Controller {
     @DeleteMapping("deleteStudent")
     public void deleteStudent(@RequestParam("id") Long id) {
         studentRepository.deleteById(id);
+    }
+
+    @GetMapping("findStudent")
+    public List<Student> findStudent(@RequestParam(value = "id", required = false) Long id,
+                                     @RequestParam(value = "name", required = false) String name,
+                                     @RequestParam(value = "classId", required = false) Long classId) {
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll()
+                .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+        Student student = new Student();
+        if (id != null) {
+            student.setId(id);
+        }
+        if (name != null) {
+            student.setName(name);
+        }
+        if (classId != null) {
+            student.setClassId(classId);
+        }
+        Example<Student> example = Example.of(student, exampleMatcher);
+        return studentRepository.findAll(example);
     }
 }
